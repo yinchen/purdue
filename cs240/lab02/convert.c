2b5c932ae707afa933497adb6b989a6bc9f0f739
Matt McCormick <mbmccormick@gmail.com>
2011-01-30 18:35:26 -0500

    Finished changes for Lab 2.

--- cs240/lab02/convert.c
+++ cs240/lab02/convert.c
@@ -2,11 +2,8 @@
  * convert.c
  * Lab 2
  *
- * Your next task for this lab is to write a program that when 
- * provided with a month (first argument) and an year (second 
- * argument), outputs the calender for that month as shown in 
- * the example above. Make sure that you print the correct 
- * number of days (28, 29, 30 or 31) for each month.
+ * In this lab you will create a program to convert a number 
+ * from one base system to another base system.
  * 
  * @author Matt McCormick (mbmccorm@purdue.edu)
  * @section R1130
@@ -18,17 +15,76 @@
 #include <string.h>
 #include <math.h>
 
+void validateBase(char num[], int frombase, int tobase);
 int convertToBase10(char num[], int base);
-char convertFromBase10(int num, int base);
+void convertFromBase10(int num, int base);
 
 main (int argc, char *argv[])
 {
+    if (argv[1] == NULL ||
+        argv[2] == NULL ||
+        argv[3] == NULL)
+    {
+        printf("Usage convert <number> <from_base> <to_base>\n");
+        exit(0);
+    }
+    
     int num, frombase, tobase;
     num = atoi(argv[1]);
     frombase = atoi(argv[2]);
     tobase = atoi(argv[3]);
     
-    printf("%d\n", convertFromBase10(num, tobase));
+    validateBase(argv[1], frombase, tobase);
+    
+    int tmp;
+    tmp = convertToBase10(argv[1], frombase);
+    
+    convertFromBase10(tmp, tobase);
+}
+
+void validateBase(char num[], int frombase, int tobase)
+{
+    if (frombase < 2 ||
+        frombase > 35)
+    {
+        printf("The bases should lie between 2 and 35\n");
+        exit(0);
+    }
+    
+    if (tobase < 2 ||
+        tobase > 35)
+    {
+        printf("The bases should lie between 2 and 35\n");
+        exit(0);
+    }
+    
+    if (strlen(num) > 32)
+    {
+        printf("The entered number is longer than 32 digits, please enter a smaller number\n");
+        exit(0);
+    }
+    
+    if (frombase < 10)
+    {
+        int i;
+        i = 0;
+        do
+        {
+            if (num[i] == 'a' ||
+                num[i] == 'b' ||
+                num[i] == 'c' ||
+                num[i] == 'd' ||
+                num[i] == 'e' ||
+                num[i] == 'f')
+            {
+                printf("Quitting for %c with base %d\n", num[i], frombase);
+                printf("The entered number is invalid for the given base\n");
+                exit(0);
+            }
+            
+            i++;
+        } while (num[i] != '\0');
+    }
 }
 
 int convertToBase10(char num[], int base)
@@ -48,15 +104,25 @@ int convertToBase10(char num[], int base)
     return retval;
 }
 
-char convertFromBase10(int num, int base)
+void convertFromBase10(int num, int base)
 {
-    int result;
-    result = num / base;
+    int result[64];
+    char base_digits[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
     
-    do
+    int i, j;
+    i = 0;
+    
+    while (num != 0)
     {
-        printf("%d\n", num % result);
-        result = result / base;
-        
-    } while (result != 0);
+        result[i] = num % base;
+        num = num / base;
+        i++;
+    }
+    
+    for(j = i - 1; j>=0; j--)
+    {
+        printf("%c", base_digits[result[j]]);
+    }
+    
+    printf("\n");
 }
