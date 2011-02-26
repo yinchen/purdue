83b6c7b3503534b7de6c46f27fe8769245f47556
Matt McCormick <mbmccormick@gmail.com>
2011-02-25 22:48:54 -0500

    Added code for Part 1 of Project 2.

--- cs240/project02/trapezoid.c
+++ cs240/project02/trapezoid.c
@@ -1,8 +1,32 @@
-/* A program that evaluates integrals based on the composite trapezoidal rule */
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_N 100
 
 int main()
 {
+	int n;
+	scanf("%d", &n);
 	
+	int* x;
+	x = (int*) malloc(n * sizeof(int));
+	int* y;
+    y = (int*) malloc(n * sizeof(int));
+    
+	int i;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d %d", &x[i], &y[i]);
+    }
+    
+    float sum;
+    sum = 0.0;
+    
+    for (i = 1; i < n; i++)
+    {
+        sum = sum + ((x[i] - x[i-1]) * (y[i] + y[i-1]));
+    }
+    sum = 0.5 * sum;
+    
+    printf("The value of the integral is %f\n", sum);
 }
