55dbb9d86507858ba2e07e25201538d273785d3e
Matt McCormick <mbmccormick@gmail.com>
2011-08-25 18:59:26 -0400

    Implemented mystrstr() for Lab 1.

--- cs251/lab01/mystring.c
+++ cs251/lab01/mystring.c
@@ -13,64 +13,93 @@
 
 char *mystrcpy(char * s1, const char * s2)
 {
-	int i;
-	i = 0;
-	
-	while(s2[i] != '\0')
-	{
-		s1[i] = s2[i];
-		i++;
-	}
-	
-	s1[i] = '\0';
-	
-	return s1;
+    int i;
+    i = 0;
+
+    while(s2[i] != '\0')
+    {
+        s1[i] = s2[i];
+        i++;
+    }
+
+    s1[i] = '\0';
+
+    return s1;
 }
 
 size_t mystrlen(const char *s)
 {
-	int i;
-	i = 0;
-	
-	while(s[i] != '\0')
-	{
-		i++;
-	}
-	
-	return i;
+    int i;
+    i = 0;
+    
+    while(s[i] != '\0')
+    {
+        i++;
+    }
+    
+    return i;
 }
 
 char *mystrdup(const char *s1)
 {
-	int len = mystrlen(s1) + 1;
-	void *new = malloc(len);
+    int len = mystrlen(s1) + 1;
+    void *new = malloc(len);
 
-  	return mystrcpy(new, s1);
+    return mystrcpy(new, s1);
 }
 
 char *mystrcat(char * s1, const char * s2)
 {
-	int len1 = mystrlen(s1) + 1;
-	int len2 = mystrlen(s2) + 1;
-	
-	s1 = realloc(s1, len2);
-	
-	int i;
-	for (i = len1; i < (len1 + len2); i++)
-	{
-		s1[i] = s2[len2 - i];
-	}
-	
-	return s1;
+    int len1 = mystrlen(s1) + 1;
+    int len2 = mystrlen(s2) + 1;
+    
+    s1 = realloc(s1, len2);
+    
+    int i;
+    for (i = len1; i < (len1 + len2); i++)
+    {
+        s1[i] = s2[len2 - i];
+    }
+    
+    return s1;
 }
 
 char *mystrstr(char * s1, const char * s2)
 {
-	
+    int len1 = mystrlen(s1);
+    int len2 = mystrlen(s2);
+    
+    int match;
+    match = 1;
+    
+    int i;
+    for (i = 0; i < len1; i++) // loop through s1
+    {
+        if (s1[i] == s2[0])
+        {
+            int j;
+            for (j = i; j < len2; j++) // loop through s2 
+            {
+                if (s1[j] != s2[j])
+                {
+                    match = 0;
+                }
+            }
+        }
+    }
+    
+    if (match)
+    {
+        return s1+i;
+    }
+    else
+    {
+        return NULL;
+    }
 }
 
 int mystrcmp(const char *s1, const char *s2)
 {
-	
+    
 }
 
