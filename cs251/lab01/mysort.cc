46bf2b77c9b5e9fd4d496f0c6a7ad47a2b60fa9c
Matt McCormick <mbmccormick@gmail.com>
2011-08-27 16:42:34 -0400

    Implemented mysort.cc requirements in C syntax, completed Makefile.

--- cs251/lab01/mysort.cc
+++ cs251/lab01/mysort.cc
@@ -15,12 +15,48 @@
 //
 // See test_sort to see how to use mysort.
 //
-void mysort( int n,                      // Number of elements
-	     int elementSize,            // Size of each element
-	     void * array,               // Pointer to an array
-	     int ascending,              // 0 -> descending; 1 -> ascending
-	     CompareFunction compFunc )  // Comparison function.
+void mysort(int n,                      // Number of elements
+            int elementSize,            // Size of each element
+            void * array,               // Pointer to an array
+            int ascending,              // 0 -> descending; 1 -> ascending
+            CompareFunction compFunc)   // Comparison function.
 {
-  // Add your code here. Use any sorting algorithm you want.
+    void *tmp;
+    tmp = alloca(elementSize);
+    
+    int changed;
+    changed = 1;
+    
+    while (changed)
+    {
+        changed = 0;
+        
+        int i;
+        for (i = 0; i < n; i++)
+        {
+            if (ascending)
+            {
+                if (compFunc(array[i], array[i + 1]) <= 0)
+                {
+                    tmp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = tmp;
+                    
+                    changed = 1;
+                }
+            }
+            else
+            {
+                if (compFunc(array[i], array[i + 1]) >= 0)
+                {
+                    tmp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = tmp;
+                    
+                    changed = 1;
+                }
+            }
+        }
+    }
 }
 
--- cs251/lab01/mystring.c
+++ cs251/lab01/mystring.c
@@ -1,6 +1,5 @@
 #include <sys/types.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 /*
  * Implement the following string procedures.
