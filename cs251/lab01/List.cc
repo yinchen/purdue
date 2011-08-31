d50fa3056ee1a3d1bafa27e94345f465530cb117
Matt McCormick <mbmccormick@gmail.com>
2011-08-31 15:08:24 -0400

    Completed List.cc requirements.

--- cs251/lab01/List.cc
+++ cs251/lab01/List.cc
@@ -4,16 +4,41 @@
 
 #include <stdio.h>
 #include "List.h"
-#include <cstdlib>
+#include <stdlib.h>
 
 //
 // Inserts a new element with value "val" in
 // ascending order.
 //
 void
-List::insertSorted( int val )
+List::insertSorted(int val)
 {
-  // Complete procedure 
+    ListNode *curr;
+    curr = _head;
+    
+    ListNode *last;
+    last = NULL;
+    
+    while (curr != NULL)
+    {
+        if (val <= curr->_value)
+        {
+            break;
+        }  
+        
+        last = curr;        
+        curr = curr->_next;
+    }
+    
+    ListNode *n;
+    n = (ListNode*)malloc(sizeof(ListNode));
+    n->_value = val;
+    n->_next = curr;
+    
+    if (last != NULL)
+        last->_next = n;
+    else
+        _head = n;
 }
 
 //
@@ -21,21 +46,25 @@ List::insertSorted( int val )
 // the end of the list.
 //
 void
-List::append( int val )
+List::append(int val)
 {
     ListNode *curr;
     curr = _head;
     
-    while (curr->_next != NULL)
+    while (curr != NULL)
     {
         curr = curr->_next;
     }
     
-    ListNode n;
-    n._value = val;
-    n._next = NULL;
+    ListNode *n;
+    n = (ListNode*)malloc(sizeof(ListNode));
+    n->_value = val;
+    n->_next = NULL;
     
-    curr->_next = &n;
+    if (curr != NULL)
+        curr->_next = n;
+    else
+        _head = n;
 }
 
 //
@@ -43,21 +72,47 @@ List::append( int val )
 // the beginning of the list.
 //
 void
-List::prepend( int val )
+List::prepend(int val)
 {
-    ListNode n;
-    n._value = val;
-    n._next = _head;
+    ListNode *n;
+    n = (ListNode*)malloc(sizeof(ListNode));
+    n->_value = val;
+    n->_next = _head;
     
-    _head = &n;
+    _head = n;
 }
 
 // Removes an element with value "val" from List
 // Returns 0 if succeeds or -1 if it fails
 int 
-List::remove( int val )
+List::remove(int val)
 {
-    return 0;
+    ListNode *curr;
+    curr = _head;
+    
+    ListNode *last;
+    last = NULL;
+    
+    if (curr->_value == val)
+    {
+        _head = curr->_next;
+        return 0;
+    }
+    
+    while (curr != NULL)
+    {
+        if (curr->_value == val)
+        {
+            last->_next = curr->_next;
+            
+            return 0;
+        }
+        
+        last = curr;        
+        curr = curr->_next;
+    }
+    
+    return -1;
 }
 
 // Prints The elements in the list. 
@@ -70,6 +125,8 @@ List::print()
     while (curr != NULL)
     {
         printf("%d\n", curr->_value);
+        
+        curr = curr->_next;
     }
 }
 
@@ -100,10 +157,7 @@ List::lookup(int val)
 //
 List::List()
 {
-    ListNode curr;
-    curr = (ListNode*)std::malloc(sizeof(ListNode));
-    
-    _head = curr;
+    _head = NULL;
 }
 
 //
@@ -119,7 +173,7 @@ List::~List()
         ListNode* next;
         next = curr->_next;
         
-        std::free(curr);
+        free(curr);
         
         curr = next;
     }
