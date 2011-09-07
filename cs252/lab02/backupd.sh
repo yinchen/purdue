0383e391aa64f8b5096b0ee85e28d9e1d6edbf25
Matt McCormick <mbmccormick@gmail.com>
2011-09-07 18:52:19 -0300

    Edited cs252/lab02/backupd.sh via GitHub

--- cs252/lab02/backupd.sh
+++ cs252/lab02/backupd.sh
@@ -2,8 +2,8 @@
 
 if [ $# -ne 4 ]
 then
-	echo "Usage: backupd.sh dir backupdir interval-secs max-backups"
-	exit 65
+    echo "Usage: backupd.sh dir backupdir interval-secs max-backups"
+    exit 65
 fi
 
 ls -lR $1 > ls-lR.last
@@ -15,25 +15,30 @@ COUNT=1
 
 while [ 1 ];
 do
-	sleep $3
-	 
-	ls -lR $1 > ls-lR.new
-	diff ls-lR.new ls-lR.last
-	
-	if [ $? -eq 1 ]
-	then
-	    DATE=$(date +"%Y-%m-%d-%H-%M-%S")
-		cp -r $1 $2/$DATE
-		
-		COUNT=$(expr $COUNT + 1)
-		
-		mv ls-lR.new ls-lR.last
-	fi
-	
-	if [ $COUNT -gt $4 ]
-	then
-	    rm -R $2/$(ls -lt $2 | grep '^d' | tail -1  | tr " " "\n" | tail -1)
-	    COUNT=$(expr $COUNT - 1)
-	fi
+    sleep $3
+     
+    ls -lR $1 > ls-lR.new
+    diff ls-lR.new ls-lR.last
+    
+    if [ $? -eq 1 ]
+    then
+        DATE=$(date +"%Y-%m-%d-%H-%M-%S")
+        cp -r $1 $2/$DATE
+        
+        COUNT=$(expr $COUNT + 1)
+        
+        mv ls-lR.new ls-lR.last
+        
+        echo "This message is to inform you that a backup has taken place on $HOST for $USER. The directory $1 was backed up to $2 on $(date).\n" > tmp_msg
+        echo "\n" >> tmp_msg
+        echo "--\n" >> tmp_msg
+        echo "backupd.sh\n" >> tmp_msg
+        /usr/bin/mailx -s "New Backup for $USER" $USER < tmp-message
+    fi
+    
+    if [ $COUNT -gt $4 ]
+    then
+        rm -R $2/$(ls -lt $2 | grep '^d' | tail -1  | tr " " "\n" | tail -1)
+        COUNT=$(expr $COUNT - 1)
+    fi
 done
-
