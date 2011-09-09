096ce35929d2f450fe9af933c5bedcf70cbbf8b8
Matt McCormick <mbmccormick@gmail.com>
2011-09-09 13:48:59 -0300

    Edited cs252/lab02/backupd.sh via GitHub

--- cs252/lab02/backupd.sh
+++ cs252/lab02/backupd.sh
@@ -11,8 +11,6 @@ ls -lR $1 > ls-lR.last
 DATE=$(date +"%Y-%m-%d-%H-%M-%S")
 cp -r $1 $2/$DATE
 
-COUNT=1
-
 while [ 1 ];
 do
     sleep $3
@@ -25,21 +23,20 @@ do
         DATE=$(date +"%Y-%m-%d-%H-%M-%S")
         cp -r $1 $2/$DATE
         
-        COUNT=$(expr $COUNT + 1)
-        
         mv ls-lR.new ls-lR.last
     	
-		echo "This message is to inform you that a backup has taken place on $HOST for $USER. The directory '$1' was backed up to '$2' on $(date)." > tmp_msg
-		echo "" >> tmp_msg
-		echo "--" >> tmp_msg
-		echo "backupd.sh" >> tmp_msg
-		echo "UNIX Backup Daemon" >> tmp_msg
-		/usr/bin/mailx -s "New Backup on $HOST for $USER" $USER < tmp_msg
+        echo "This message is to inform you that a backup has taken place on $HOST for $USER. The directory '$1' was backed up to '$2' on $(date)." > tmp_msg
+        echo "" >> tmp_msg
+        echo "--" >> tmp_msg
+        echo "backupd.sh" >> tmp_msg
+        echo "UNIX Backup Daemon" >> tmp_msg
+        /usr/bin/mailx -s "New Backup on $HOST for $USER" $USER < tmp_msg
     fi
     
+    COUNT=$(ls -l $2 | grep ^d | wc -l | tr -s " " | tr -d " ")
+
     if [ $COUNT -gt $4 ]
     then
         rm -R $2/$(ls -lt $2 | grep '^d' | tail -1  | tr " " "\n" | tail -1)
-        COUNT=$(expr $COUNT - 1)
     fi
 done
