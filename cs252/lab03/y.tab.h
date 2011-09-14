cafb32cad76a987c53648883d23ab2784ac15923
Matt McCormick <mbmccormick@gmail.com>
2011-09-14 12:39:21 -0400

    Adding code for Lab 3.

---
+++ cs252/lab03/y.tab.h
@@ -0,0 +1,13 @@
+
+typedef union
+#ifdef __cplusplus
+	YYSTYPE
+#endif
+	{
+		char   *string_val;
+	} YYSTYPE;
+extern YYSTYPE yylval;
+# define WORD 257
+# define NOTOKEN 258
+# define GREAT 259
+# define NEWLINE 260
