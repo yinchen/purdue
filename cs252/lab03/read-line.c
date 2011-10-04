62b0af3cb4df4e63b6a43011fb8ea91a3e2bf161
Matt McCormick <mbmccormick@gmail.com>
2011-10-04 14:07:30 -0300

    Fixing spelling mistake.

--- cs252/lab03/read-line.c
+++ cs252/lab03/read-line.c
@@ -168,7 +168,7 @@ char * read_line()
                 // echo line
                 write(1, line_buffer, line_length);
                 
-                curs_post = line_length;
+                curs_pos = line_length;
             }
             else if (ch1==91 && ch2==68)
             {
