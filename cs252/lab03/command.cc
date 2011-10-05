c3fe612450970c40921c7a4f7511899e8a482556
Matt McCormick <mbmccormick@gmail.com>
2011-10-05 11:42:33 -0300

    Edited cs252/lab03/command.cc via GitHub

--- cs252/lab03/command.cc
+++ cs252/lab03/command.cc
@@ -379,6 +379,8 @@ extern "C" void disp(int sig)
 
 extern "C" void killzombie(int sig)
 {
+    return; // fix this
+    
     int pid = wait3(0, 0, NULL);
     if (pid <= 0)
         while(waitpid(-1, NULL, WNOHANG) > 0);
