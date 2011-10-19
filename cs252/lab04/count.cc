7538dfe44eb1c58c0a8b8c6b32e712432b7fdf28
Matt McCormick <mbmccormick@gmail.com>
2011-10-19 14:04:54 -0400

    Adding completed Part 1 and Part 2 of Lab 4.

---
+++ cs252/lab04/count.cc
@@ -0,0 +1,61 @@
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int count;
+
+pthread_mutex_t mt;
+pthread_mutexattr_t mattr;
+
+void increment( int ntimes )
+{
+	pthread_mutex_lock( &mt );
+	
+	for ( int i = 0; i < ntimes; i++ ) {
+		int c;
+
+		c = count;
+		c = c + 1;
+
+		count = c;
+	}
+	
+	pthread_mutex_unlock( &mt );
+}
+
+int main( int argc, char ** argv )
+{
+	int n = 10000000;
+	pthread_t t1, t2;
+    pthread_attr_t attr;
+    
+    pthread_mutexattr_init( &mattr );
+    pthread_mutex_init( &mt, &mattr );
+
+    pthread_attr_init( &attr );
+    pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+
+	printf("Start Test. Final count should be %d\n", 2 * n );
+
+	// Create threads
+	pthread_create( &t1, &attr, (void * (*)(void *)) increment, 
+			(void *) n);
+
+	pthread_create( &t2, &attr, (void * (*)(void *)) increment, 
+			(void *) n);
+
+	// Wait until threads are done
+	pthread_join( t1, NULL );
+	pthread_join( t2, NULL );
+
+	if ( count != 2 * n ) {
+		printf("\n****** Error. Final count is %d\n", count );
+		printf("****** It should be %d\n", 2 * n );
+	}
+	else {
+		printf("\n>>>>>> O.K. Final count is %d\n", count );
+	}
+}
+
+
