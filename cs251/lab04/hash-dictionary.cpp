e35bc431d756dfed8450e4492329c20b01d02908
Matt McCormick <mbmccormick@gmail.com>
2011-10-28 14:22:39 -0400

    Adding completed HashTable implementation for Part 6 of Lab 4.

--- cs251/lab04/hash-dictionary.cpp
+++ cs251/lab04/hash-dictionary.cpp
@@ -8,32 +8,73 @@
 // Constructor
 HashDictionary::HashDictionary()
 {
-  // Add your code here
+	int i;
+	for (i = 0; i < MaxBuckets; i++)
+		buckets[i] = NULL;
 }
 
 int
-HashDictionary::hash(KeyType key) {
-  // Add your code here
-  int sum = 0;
-  int len = strlen(key);
-  for (int i = 0; i < len; i++) {
+HashDictionary::hash(KeyType key)
+{
+	int sum = 0;
+	int len = strlen(key);
+	for (int i = 0; i < len; i++)
+	{
 		sum += i*key[i];
-  }
-  return sum%MaxBuckets;
+	}
+	return sum%MaxBuckets;
 }
 
 // Add a record to the dictionary. Returns false if key already exists
 bool
-HashDictionary::addRecord( KeyType key, DataType record)
+HashDictionary::addRecord(KeyType key, DataType record)
 {
-  // Add your code here
-  return false;
+	int h = hash(key);
+	
+	HashNode *e;
+	e = buckets[h];
+	
+	while (e != NULL)
+	{
+		if (strcmp(e->key, key) == 0)
+		{
+			e->data = record;
+			return false;
+		}
+		
+		e = e->next;
+	}
+	
+	e = new HashNode();
+	e->key = strdup(key);
+	e->data = record;
+	e->next = buckets[h];
+	
+	buckets[h] = e;
+	nElements++;
+	
+	return true;
 }
 
 // Find a key in the dictionary and return corresponding record or NULL
 DataType
-HashDictionary::findRecord( KeyType key)
+HashDictionary::findRecord(KeyType key)
 {
+	int h = hash(key);
+	
+	HashNode *curr;
+	curr = buckets[h];
+	
+	while (curr != NULL)
+	{
+		if (strcmp(curr->key, key) == 0)
+		{
+			return (DataType)curr->data;
+		}
+		
+		curr = curr->next;
+	}
+	
 	return NULL;
 }
 
@@ -41,8 +82,38 @@ HashDictionary::findRecord( KeyType key)
 bool
 HashDictionary::removeElement(KeyType key)
 {
-  // Add your code here
-	return true;
+	int h = hash(key);
+	
+	HashNode *curr;
+	curr = buckets[h];
+	
+	HashNode *prev;
+	prev = NULL;
+	
+	while (curr != NULL)
+	{
+		if (strcmp(curr->key, key) == 0)
+		{
+			if (prev != NULL)
+			{
+				prev->next = curr->next;
+			}
+			else
+			{
+				buckets[h] = curr->next;
+			}
+			
+			nElements--;
+			
+			delete curr;
+			return true;
+		}
+		
+		prev = curr;
+		curr = curr->next;
+	}
+	
+	return false;
 }
 
 // Returns all the elements in the table as an array of strings.
@@ -65,4 +136,3 @@ HashDictionary::keys(int * n)
 	*n = nElements;
 	return a;
 }
-
