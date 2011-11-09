e4612ff404b081e336e72a977f00847d95c05ef8
Matt McCormick <mbmccormick@gmail.com>
2011-11-09 10:44:03 -0500

    Update cs251/lab05/Location.java

--- cs251/lab05/Location.java
+++ cs251/lab05/Location.java
@@ -21,66 +21,65 @@ class Location extends JPanel
 {
     private String _name;
     private int _id, _x, _y;
-	
-	public Location() {}
-	
-	public Location(int x, int y)
+    
+    public Location() {}
+    
+    public Location(int x, int y)
     {
-		_x = x;
-		_y = y;
-	}
-	
-	public Location(String name, int id, int x, int y)
+        _x = x;
+        _y = y;
+    }
+    
+    public Location(String name, int id, int x, int y)
     {
-		_name = name;
-    	_id = id;
-		_x = x;
-		_y = y;
-	}
-	
+        _name = name;
+        _id = id;
+        _x = x;
+        _y = y;
+    }
+    
     public String getName()
     {
-    	return _name;
-	}
+        return _name;
+    }
     
-	public void setName(String name)
+    public void setName(String name)
     {
-		_name = name;
-	}
-	
+        _name = name;
+    }
+    
     public int getID()
     {
-		return _id;
-	}
+        return _id;
+    }
     
-	public void setID(int id)
+    public void setID(int id)
     {
-		_id = id;
-	}	
-	
-	public int getX()
-    {
-		return _x;
-	}
+        _id = id;
+    }    
     
-	public void setX(int x)
+    public int getX()
     {
-		_x = x;
-	}
-	
-	public int getY()
+        return _x;
+    }
+    
+    public void setX(int x)
     {
-		return _y;
-	}
-	
-	public void setY(int y)
+        _x = x;
+    }
+    
+    public int getY()
     {
-		_y = y;
-	}	
-	
-	public void paint(Graphics g)
+        return _y;
+    }
+    
+    public void setY(int y)
     {
-		g.fillOval(x - 8, y - 8, 16, 16);
-	}
+        _y = y;
+    }    
+    
+    public void paint(Graphics g)
+    {
+        g.fillOval(x - 8, y - 8, 16, 16);
+    }
 }
-
