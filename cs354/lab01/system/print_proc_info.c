de1bdde7bcc25ac60d3cf50daa527ebbb2c0914a
Matt McCormick <mbmccormick@gmail.com>
2012-01-30 16:41:54 -0500

    Adding more code for Lab 1.

--- cs354/lab01/system/print_proc_info.c
+++ cs354/lab01/system/print_proc_info.c
@@ -9,16 +9,21 @@ syscall print_proc_info(pid32 pid)
     intmask mask; // saved interrupt mask
     mask = disable();
     
-    if ((ssize < MINSTK) || 
-        (priority <= 0) || 
-        (((int32)(pid = newpid())) == (int32) SYSERR) || 
-        ((saddr = (uint32 *)getstk(ssize)) == (uint32 *)SYSERR))
-    {
-        return SYSERR;
-    }
-
-	prcount++;
-	prptr = &proctab[pid];
+    
+    prptr = &proctab[pid];
+	if (prptr == NULL)
+	{
+	    return SYSERR;
+	}
+	
+	kprintf(prptr->prname);
+	kprintf(" ");
+	kprintf(prptr->prprio);
+	kprintf(" ");
+	kprintf(prptr->prstate);
+	kprintf(" ");
+	kprintf(prptr->prstkptr);
+	kprintf("\n");
 
     restore(mask);
 }
