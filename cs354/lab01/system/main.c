faff7dfd11603c7d917a40247284ea132b486fba
Matt McCormick <mbmccormick@gmail.com>
2012-01-31 17:22:14 -0500

    Updating code for Lab 1 to add 2 sleeper processes.

--- cs354/lab01/system/main.c
+++ cs354/lab01/system/main.c
@@ -15,14 +15,16 @@ int main(int argc, char **argv)
     kprintf("\r\n");
 
     uint32 i;
+    pid32 proc1;
 
-    resume(create((void*)sleep(10), 200, 20, "sleeper 1", 0));
-    resume(create((void*)sleep(10), 200, 20, "sleeper 2", 0));
+    proc1 = create((void*)sleep(10), 200, 20, "sleeper", 0);
     
     for (i = 0; i < NPROC; i++)
     {
         print_proc_info(i);
     }
+
+    resume(proc1);
 
     return OK;
 }
