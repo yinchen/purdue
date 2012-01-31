06b6b81c10b643eb42f1f7df75db4ca171a02ec3
Matt McCormick <mbmccormick@gmail.com>
2012-01-31 09:54:37 -0500

    Updating code for Lab 1.

--- cs354/lab01/system/main.c
+++ cs354/lab01/system/main.c
@@ -4,14 +4,21 @@
 #include <stdio.h>
 
 /************************************************************************/
-/*									*/
-/* main - main program for testing Xinu					*/
-/*									*/
+/*                                                                      */
+/* main - main program for testing Xinu                                 */
+/*                                                                      */
 /************************************************************************/
 
 int main(int argc, char **argv)
 {
-	kprintf("Hello World!\n");
-	
-	return OK;
+    kprintf("Hello World!\n\n");
+
+    uint32 i;
+
+    for (i = 0; i < NPROC; i++)
+    {
+        print_proc_info(i);
+    }
+    
+    return OK;
 }
--- cs354/lab01/system/print_proc_info.c
+++ cs354/lab01/system/print_proc_info.c
@@ -6,31 +6,23 @@
 syscall print_proc_info(pid32 pid)
 {
     intmask mask;
-    struct	procent *prptr;
+    struct procent *prptr;
     
     // save interrupt mask
     mask = disable();
 
     // check if PID is valid
     prptr = &proctab[pid];
-	if (pid > NPROC || prptr == NULL)
-	{
-	    return SYSERR;
-	}
-	
-	// print PID information
-	kprintf(prptr->prname);
-	kprintf(" ");
-	kprintf(prptr->prprio);
-	kprintf(" ");
-	kprintf(prptr->prstate);
-	kprintf(" ");
-	kprintf(prptr->prstkptr);
-	kprintf("\n");
+    if (pid > NPROC || prptr == NULL)
+    {
+        return SYSERR;
+    }
+    
+    // print PID information
+    kprintf("%s %d %d %s\n", prptr->prname, prptr->prprio, prptr->prstate, prptr->prstkptr);
 
-	// restore interrupt mask
+    // restore interrupt mask
     restore(mask);
 
     return OK;
 }
-
