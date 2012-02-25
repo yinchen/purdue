ba74bcf96ac31c91bf5d6ec950621c5234018a54
Matt McCormick <mbmccormick@gmail.com>
2012-02-25 15:21:11 -0500

    Adding pipdelete() syscall.

---
+++ cs354/lab03/system/pipdelete.c
@@ -0,0 +1,27 @@
+/* pipdelete.c - pipdelete */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ * pipdelete  --  Delete a pipe by releasing its table entry
+ *------------------------------------------------------------------------
+ */
+syscall	pipdelete(
+	  pipid32		pip		/* ID of semaphore to delete	*/
+	)
+{
+	if (isbadpip(pip)) {
+		return SYSERR;
+	}
+	
+	pipptr = &piptab[pip];
+	if (pipptr->powner != currpid) {
+		return SYSERR;
+	}
+	if (pipptr->pstate == PIPE_FREE) {
+		return SYSERR;
+	}
+	pipptr->pstate = PIPE_FREE;
+
+	return OK;
+}
