fb6c073d5e1c7e3ad28a02156df79a72ad55adea
Matt McCormick <mbmccormick@gmail.com>
2012-04-03 18:50:09 -0400

    Adding debugging output to main().

--- cs354/lab04/system/main.c
+++ cs354/lab04/system/main.c
@@ -13,19 +13,31 @@ void processB(pid32 prA)
 {
 	while(1)
 	{
-	    kprintf("%d", receiveb());
+		kprintf("Receiving message.\r\n");
+
+	    kprintf("RCV: %d", receiveb());
+
+	    kprintf("Message received.\r\n");
     }
 }
 
 int main(int argc, char **argv)
 {
+	kprintf("Creating new process.\r\n");
+
 	pid32 prB = create(processB, 500, 20, "processB", 1, currpid);
 	resume(prB);
 
 	while (1)
 	{
+		kprintf("Sending message.\r\n");
+
 		sendb(prB, 4);
+
+		kprintf("Message sent.\r\n");	
 	}
+
+	kprintf("Exiting.\r\n");
 
 	return OK;
 }
