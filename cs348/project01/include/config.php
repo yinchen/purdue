4d04e3f1482197fdaea675eb22f064d0111a58f9
Matt McCormick <mbmccormick@gmail.com>
2012-04-20 18:31:35 -0400

    Error handling.

--- cs348/project01/include/config.php
+++ cs348/project01/include/config.php
@@ -6,7 +6,7 @@
 	$con = oci_connect("mbmccorm@csora", "veStU554UChA", "claros.cs.purdue.edu:1524/strep");
 	if (!$con)
 	{
-		die("Could not connect: " . oci_error());
+		// die("Could not connect: " . oci_error());
 
 		$e = oci_error();
     	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
