6479f74ec4853c6325760ab96a8da92b65d1099a
Matt McCormick <mbmccormick@gmail.com>
2012-04-20 19:12:06 -0400

    Fixing TNS directive.

--- cs348/project01/include/config.php
+++ cs348/project01/include/config.php
@@ -2,7 +2,7 @@
 
 	$RootDirectory = "/cgi-bin/mbmccorm/348/";
 
-	$con = oci_connect("mbmccorm", "veStU554UChA", "claros.cs.purdue.edu:1524/strep");
+	$con = oci_connect("mbmccorm", "veStU554UChA", "csora");
 	if (!$con)
 	{
 		$e = oci_error();
