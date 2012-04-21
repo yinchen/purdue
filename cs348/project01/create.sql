afa07664d32bda48942fccfcd05e4b885c95d4f2
Matt McCormick <mbmccormick@gmail.com>
2012-04-20 20:38:47 -0400

    Fixing SQL.

--- cs348/project01/create.sql
+++ cs348/project01/create.sql
@@ -1,16 +1,16 @@
+DROP TABLE CourseEvaluations CASCADE CONSTRAINTS;
 CREATE TABLE CourseEvaluations (
   EvaluationID integer NOT NULL,
-  CourseID integer DEFAULT NULL,
+  CourseID integer DEFAULT NULL REFERENCES Courses (CourseID),
   EvaluationName varchar(50) DEFAULT NULL,
   Type integer DEFAULT NULL,
   Weightage number(2,2) DEFAULT NULL,
   DeadlineDate datetime DEFAULT NULL,
   MeetingRoom varchar(50) DEFAULT NULL,
-  PRIMARY KEY (EvaluationID),
-  KEY FK_CourseEvaluations_CourseID (CourseID),
-  CONSTRAINT FK_CourseEvaluations_CourseID FOREIGN KEY (CourseID) REFERENCES Courses (CourseID)
+  PRIMARY KEY (EvaluationID)
 );
 
+DROP TABLE Courses CASCADE CONSTRAINTS;
 CREATE TABLE Courses (
   CourseID integer NOT NULL,
   CourseName varchar(50) DEFAULT NULL,
@@ -18,21 +18,18 @@ CREATE TABLE Courses (
   Year integer DEFAULT NULL,
   Meets_At varchar(50) DEFAULT NULL,
   Room varchar(50) DEFAULT NULL,
-  FacultyID integer DEFAULT NULL,
-  PRIMARY KEY (CourseID),
-  KEY FK_Courses_FacultyID (FacultyID),
-  CONSTRAINT FK_Courses_FacultyID FOREIGN KEY (FacultyID) REFERENCES Faculties (FacultyID)
+  FacultyID integer DEFAULT NULL REFERENCES Faculties (FacultyID),
+  PRIMARY KEY (CourseID)
 );
 
+DROP TABLE CourseStudents CASCADE CONSTRAINTS;
 CREATE TABLE CourseStudents (
-  CourseID integer NOT NULL DEFAULT '0',
-  StudentID integer NOT NULL DEFAULT '0',
-  PRIMARY KEY (CourseID,StudentID),
-  KEY FK_CourseStudents_StudentID (StudentID),
-  CONSTRAINT FK_CourseStudents_CourseID FOREIGN KEY (CourseID) REFERENCES Courses (CourseID),
-  CONSTRAINT FK_CourseStudents_StudentID FOREIGN KEY (StudentID) REFERENCES Students (StudentID)
+  CourseID integer NOT NULL DEFAULT '0' REFERENCES Courses (CourseID),
+  StudentID integer NOT NULL DEFAULT '0' REFERENCES Students (StudentID),
+  PRIMARY KEY (CourseID,StudentID)
 );
 
+DROP TABLE Departments CASCADE CONSTRAINTS;
 CREATE TABLE Departments (
   DepartmentID integer NOT NULL,
   Name varchar(50) DEFAULT NULL,
@@ -40,25 +37,23 @@ CREATE TABLE Departments (
   PRIMARY KEY (DepartmentID)
 );
 
+DROP TABLE EvaluationGrades CASCADE CONSTRAINTS;
 CREATE TABLE EvaluationGrades (
-  EvaluationID integer NOT NULL DEFAULT '0',
-  StudentID integer NOT NULL DEFAULT '0',
+  EvaluationID integer NOT NULL DEFAULT '0' REFERENCES CourseEvaluations (EvaluationID),
+  StudentID integer NOT NULL DEFAULT '0' REFERENCES Students (StudentID),
   Grade number(5,2) DEFAULT NULL,
-  PRIMARY KEY (EvaluationID,StudentID),
-  KEY FK_EvaluationGrades_StudentID (StudentID),
-  CONSTRAINT FK_EvaluationGrades_EvaluationID FOREIGN KEY (EvaluationID) REFERENCES CourseEvaluations (EvaluationID),
-  CONSTRAINT FK_EvaluationGrades_StudentID FOREIGN KEY (StudentID) REFERENCES Students (StudentID)
+  PRIMARY KEY (EvaluationID,StudentID)
 );
 
+DROP TABLE Faculties CASCADE CONSTRAINTS;
 CREATE TABLE Faculties (
   FacultyID integer NOT NULL,
-  DepartmentID integer DEFAULT NULL,
+  DepartmentID integer DEFAULT NULL REFERENCES Departments (DepartmentID),
   Name varchar(50) DEFAULT NULL,
-  PRIMARY KEY (FacultyID),
-  KEY FK_Faculties_DepartmentID (DepartmentID),
-  CONSTRAINT FK_Faculties_DepartmentID FOREIGN KEY (DepartmentID) REFERENCES Departments (DepartmentID)
+  PRIMARY KEY (FacultyID)
 );
 
+DROP TABLE Students CASCADE CONSTRAINTS;
 CREATE TABLE Students (
   StudentID integer NOT NULL,
   Name varchar(50) DEFAULT NULL,
